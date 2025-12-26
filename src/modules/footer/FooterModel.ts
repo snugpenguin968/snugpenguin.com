be388d47d06fb06f1c2f7f733e930196603ee42f
John Tian <85849926+snugpenguin968@users.noreply.github.com>
2025-12-25 21:56:48 -0600

    add beli

--- src/modules/footer/FooterModel.ts
+++ src/modules/footer/FooterModel.ts
@@ -11,10 +11,10 @@ export const useFooterModel = () => {
             address: "johntian2014@gmail.com",
             url: "mailto:johntian2014@gmail.com",
         },
-        discord: {
-            label: "Discord",
-            username: "@snugpenguin968",
-            url: "https://discord.com/users/snugpenguin968", // Assuming standard discord user link format or just a placeholder if not linkable directly
+        beli: {
+            label: "Beli",
+            username: "@snugpenguin",
+            url: "https://beliapp.co",
         },
         linkedin: {
             label: "LinkedIn",
